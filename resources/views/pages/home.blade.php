950a052a79177209c1afb107ca1ad72ef1f56223
System Administrator <root@MacBook-Air-van-kenny.local>
2016-11-22 21:26:12 +0100

    create homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,212 @@
+@extends('layout.layout')
+
+@section('hero')
+<section class="hero is-medium is-info is-bold">
+  <div class="hero-body">
+    <div class="container">
+      
+    </div>
+  </div>
+</section>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+  <section class="section has-text-centered pb-50">
+      <div class="container has-half-container-width">
+        <div class="heading">
+          <h1 class="title pb-20">Cuipers Johnny Bouwwerken</h1>
+          <hr>
+          <h2 class="subtitle">
+            Voor al uw bouwwerken, verbouwingen en nieuwbouw kan u bij ons terecht. 
+            Kwaliteit en professionaliteit komen op de eerste plaats.
+          </h2>
+          <hr>
+        </div>
+        <p class="content pt-20">Welkom op onze website
+          Onze werkzaamheden bestaan uit het aannemen en uitvoeren van <strong>algemene 
+          bouw- en renovatiewerken, verbouwingen en nieuwbouwwoningen.</strong>
+          Door een ruime ervaring, zorgen we er steeds voor dat we voor elk project, 
+          groot of klein kwalitatief werk afleveren.
+
+          Voor meer info of wenst u een vrijblijvende offerte, aarzel niet om ons te contacteren 
+          via het contactformulier.
+        
+          Alvast bedankt,
+          Met vriendelijke groeten,
+
+          Johnny Cuipers
+        </p>
+    </div>
+  </section>
+
+  <section class="section has-no-background">
+    <div class="container has-text-centered">
+
+        <div class="heading mb-50">
+          <h1 class="title">Nieuwbouw</h1>
+          <hr>
+          <h2 class="subtitle">
+            Een overzicht van realisaties nieuwbouw.
+          </h2>
+        </div>
+        
+        <div class="columns is-multiline">
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container has-text-centered">
+
+        <div class="heading mb-50">
+          <h1 class="title">Verbouwingen</h1>
+          <hr>
+          <h2 class="subtitle">
+            Een overzicht van realisaties verbouwingen.
+          </h2>
+        </div>
+        
+        <div class="columns is-multiline">
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <section class="section has-no-background">
+    <div class="container has-text-centered">
+
+        <div class="heading mb-50">
+          <h1 class="title">Renovaties</h1>
+          <hr>
+          <h2 class="subtitle">
+            Een overzicht van realisaties renovaties.
+          </h2>
+        </div>
+        
+        <div class="columns is-multiline">
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+          <div class="column is-one-third showcase-card-column">
+            <img class="showcase-card-image" src="{{ asset('images/hero2.jpg') }}" />
+            <div class="showcase-card-image-overlay">
+              <h1 class="title is-3">Waregem</h1>
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container has-text-centered">
+        <div class="heading mb-50">
+          <h1 class="title">Contact</h1>
+          <hr>
+          <h2 class="subtitle">
+            Via onderstaand formulier kan u ons contacteren voor al uw vragen
+          </h2>
+        </div>
+        
+    </div>
+  </section>
+</div>
+@endsection
---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,28 @@
+<nav class="nav has-shadow">
+    <div class="container">
+        <div class="nav-left">
+            <a class="nav-item is-brand" href="#">
+              <img src="{{ asset('images/logo.png') }}" alt="Cuipers Johnny | Bouwwerken">
+            </a>
+        </div>
+        <div class="nav-right nav-menu">
+            <a class="nav-item" href="#">
+              Nieuwbouw
+            </a>
+            <a class="nav-item" href="#">
+              Verbouwingen
+            </a>
+            <a class="nav-item" href="#">
+              Renovatie
+            </a>
+            <a class="nav-item" href="#">
+              Vacatures
+            </a>
+        </div>
+    </div>
+    <span class="nav-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+    </span>
+</nav>
