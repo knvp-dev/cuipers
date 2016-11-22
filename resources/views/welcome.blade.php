c85926bb41f47f43affd4b265835b15126773a77
webdesigner-konvert <webdesigner@konvert.be>
2016-11-22 15:55:18 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <nav class="nav has-shadow">
+            <div class="container">
+                <div class="nav-left">
+                    <a class="nav-item is-brand" href="#">
+                      <img src="http://bulma.io/images/bulma-type.png" alt="Bulma logo">
+                    </a>
+                </div>
+                <div class="nav-right nav-menu">
+                    <a class="nav-item" href="#">
+                      Home
+                    </a>
+                    <a class="nav-item" href="#">
+                      Documentation
+                    </a>
+                    <a class="nav-item" href="#">
+                      Blog
+                    </a>
+                </div>
+            </div>
+            <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+        </nav>
+
+        <section class="hero is-medium is-info is-bold">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Primary bold title <i class="fa fa-check"></i>
+              </h1>
+              <h2 class="subtitle">
+                Primary bold subtitle
+              </h2>
+            </div>
+          </div>
+        </section>
+        
+    </body>
+</html>
